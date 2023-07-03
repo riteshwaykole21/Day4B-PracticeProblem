a93e893f3d93dddc707e7f16dcd4b354c02cb823
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-07-03 23:16:39 +0530

    [Ritesh]add : Duplicate Element

---
+++ ClassProblem/DuplicateEle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProblem
+{
+    public class DuplicateEle
+    {
+        public void Elements()
+        {
+            string Sen = "ABCDEFGAC";
+            char[] Sen2 = Sen.ToCharArray();
+            int count = 0;
+            for (int i = 0; i < Sen2.Length; i++)
+            {  
+                for (int j = i + 1; j < Sen2.Length; j++)
+                {
+                    if (Sen2[i] == Sen2[j])
+                    {
+                        Console.WriteLine(Sen2[j]);
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+            }
+
+           
+        }
+    }
+}
---
+++ ClassProblem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : Duplicate element Of An Array");
+            Console.WriteLine("Enter A Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    DuplicateEle duplicateEle = new DuplicateEle();
+                    duplicateEle.Elements();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
